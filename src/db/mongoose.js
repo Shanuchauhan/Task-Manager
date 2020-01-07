777a8604ffdccacabba629875e6603e5dee09faf
Shantanu Chauhan <shantanu.chauhan29@gmail.com>
2020-01-07 20:16:09 +0530

    Integrating postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,32 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 })
 
-// const user= mongoose.model('User',{
-//     name:{
-//         type:String
-//     },
-//     age:{
-//         type:Number
-//     }
-// })
-
-const tasks= mongoose.model('Tasks',{
-    description:{
-        type:String
-    },
-    status:{
-        type:Boolean
-    }
-})
-
-
-const me=new tasks({
-    description:'Wiped the Covenant',
-    age:true
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log("Error!",error)
-})
