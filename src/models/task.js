ca84c52d21d53ca69e10437478982f4bc69f60bd
Shantanu Chauhan <shantanu.chauhan29@gmail.com>
2020-01-09 19:51:22 +0530

    Integrating User Tokens

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const validator=require('validator')
 
-const Tasks= mongoose.model('Tasks',{
+const taskSchema=new mongoose.Schema({
     description:{
         type:String,
         required:true,
@@ -12,5 +12,8 @@ const Tasks= mongoose.model('Tasks',{
         default:false
     }
 })
+
+
+const Tasks= mongoose.model('Tasks',taskSchema)
 
 module.exports = Tasks
--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
-
-const User= mongoose.model('User',{
+const userSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true,
@@ -10,6 +11,7 @@ const User= mongoose.model('User',{
     },
     email:{
         type:String,
+        unique:true,
         required:true,
         lowercase:true,
         validate(value){
@@ -32,12 +34,57 @@ const User= mongoose.model('User',{
     }, 
     age:{
         type:Number,
+        default:0,
         validate(value){
             if(value<0){
                 throw new Error('Age must be a positive number!')
             }
         }
-    }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken=async function(){
+    const user=this
+    const token=jwt.sign({_id:user._id.toString()},'thisis')
+
+    user.tokens=user.tokens.concat({token:token})
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials=async(email,password)=>{
+    const user=await User.findOne({ email })
+
+    if(!user){
+        throw new Error('Unable to Login')
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+        throw new Error('Unable to Login')
+    }
+
+    return user
+}
+
+userSchema.pre('save',async function(next){
+    const user=this
+
+    if(user.isModified('password')){
+        user.password=await bcrypt.hash(user.password,8)
+    }
+
+    next()
+})
+
+const User= mongoose.model('User',userSchema)
 
 module.exports=User
